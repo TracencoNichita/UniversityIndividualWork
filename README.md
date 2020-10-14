e52e8fd6c196a6105c17a0df96005bf33ed3c67a
Tracenco Nichita <nichita.tracenco@gmail.com>
2020-10-14 12:54:52 +0300

    new commit

---
+++ README.md
@@ -0,0 +1,6 @@
+## Installation
+1)git clone repo
+2)cd Angular-Movies
+3)npm install
+4)ng serve
+5)go to http://localhost:4200/# UniversityIndividualWork
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: 'b202c462e1bd0908be449b652eac27f5',
+    authDomain: 'YOUR-FIREBASE-DATA',
+    databaseURL: 'YOUR-FIREBASE-DATA',
+    projectId: 'YOUR-FIREBASE-DATA',
+    storageBucket: 'YOUR-FIREBASE-DATA',
+    messagingSenderId: 'YOUR-FIREBASE-DATA'
+  }
+};
